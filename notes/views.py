44e9a043d0153c38d7d534d92abdd7e368bad316
vintze <vintzeanu@gmail.com>
2024-02-06 16:17:29 +0200

    Update data

--- notes/views.py
+++ notes/views.py
@@ -1,34 +1,33 @@
+from typing import List
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Notes
+
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
-    
 
 class NotesListView(ListView):
     model = Notes
-    context_object_name = 'notes'
+    context_object_name = "notes"
     template_name = "notes/notes_list.html"
-
-# def list(request):
-#     all_notes = Notes.objects.all()
-#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
 class NotesDetailView(DetailView):
     model = Notes
-    context_object_name = 'note'
-
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Notes doesn't exist")
-    return render(request, 'notes/notes_detail.html', {'note': note})
-
-
+    context_object_name = "note"
